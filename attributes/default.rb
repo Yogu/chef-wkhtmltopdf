646e68eb04727ab4bbe867f43d2cac9322de6fc1
Jan Melcher <info@yogularm.de>
2014-10-17 02:59:06 +0200

    re-added accidentally removed dependency_packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,10 @@ default['wkhtmltopdf']['version']     = '0.12.1'
 default['wkhtmltopdf']['install_dir'] = '/usr/local/bin'
 default['wkhtmltopdf']['lib_dir']     = ''
 
+default['wkhtmltopdf']['dependency_packages'] = value_for_platform_family(
+  %w(debian) => %w(libfontconfig1 libssl0.9.8 libxext6 libxrender1),
+  %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
+)
+
 default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}.tar.bz2"
 default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['archive']}"
